e68baa526416ed8f05224e688c8a7704627e8b93
John Park <jwpark417@gmail.com>
2016-01-30 14:09:18 -0500

    Adding figaro gem

--- hoot/app/models/restaurant.rb
+++ hoot/app/models/restaurant.rb
@@ -1,10 +1,14 @@
 
 class Restaurant < ActiveRecord::Base
-<<<<<<< HEAD
-  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-=======
-  mount_uploader :image, ImageUploader
->>>>>>> 4de3d659c3a35b7275cf41be9548daf0f653ed82
+  has_attached_file :image, :styles => {
+    :medium => "200x", :thumb => "100x100>" },
+    :default_url => "default.png",
+    :storage => :dropbox,
+    :dropbox_credentials =>
+    Rails.root.join("config/dropbox.yml")
+
+    validates_attachment_content_type :image,
+    :content_type => /\Aimage\/.*\Z/
+
 end
